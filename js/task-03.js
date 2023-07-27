75fc0446cba962605e57df2b7727e23176ef14a1
TanyaJulieva <128372881+TanyaJulieva@users.noreply.github.com>
2023-07-27 16:59:55 +0300

    task-03

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,15 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const list = document.querySelector('.gallery')
+
+function createMarkup(arr) {
+  const markup = arr.map(({url, alt}) => `<li>
+  <img src="${url}" alt="${alt}" width="500">
+</li>`).join('');
+
+list.insertAdjacentHTML('beforeend', markup);
+};
+
+createMarkup(images)
