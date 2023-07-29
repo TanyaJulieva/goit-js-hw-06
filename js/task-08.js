6463ff90e1d2c16b3ed84c5680e07ed5107215f0
TanyaJulieva <128372881+TanyaJulieva@users.noreply.github.com>
2023-07-29 23:36:21 +0300

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,12 @@
+const form = document.querySelector('.login-form')
 
+form.addEventListener('submit', handlerSubmit)
+
+function handlerSubmit(evt) {
+    evt.preventDefault()
+    const {email, password} = evt.currentTarget.elements
+    if(email.value === "" || password.value === ""){
+        const message = "Всі поля повинні бути заповнені!"
+        alert(message)
+    };
+};
