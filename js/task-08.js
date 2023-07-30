b71337b60a2403a55582190f17c60a54927d7588
TanyaJulieva <128372881+TanyaJulieva@users.noreply.github.com>
2023-07-30 13:25:29 +0300

    task-09

--- js/task-08.js
+++ js/task-08.js
@@ -5,8 +5,21 @@ form.addEventListener('submit', handlerSubmit)
 function handlerSubmit(evt) {
     evt.preventDefault()
     const {email, password} = evt.currentTarget.elements
+    
     if(email.value === "" || password.value === ""){
         const message = "Всі поля повинні бути заповнені!"
-        alert(message)
+        alert(message);
+    } 
+    getObj(email, password);
+
+    console.log(getObj(email, password))
+
+    form.reset()
+};
+
+function getObj(email, password) {
+    return {
+        email: email.value,
+        password: password.value
     };
 };
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const color = document.querySelector('.color')
+const changeColorBtn = document.querySelector('.change-color')
+const body = document.querySelector('body')
+
+changeColorBtn.addEventListener('click', handlerClick)
+
+function handlerClick(evt) {
+  color.textContent = getRandomHexColor()
+  body.style.backgroundColor = color.textContent
+}
